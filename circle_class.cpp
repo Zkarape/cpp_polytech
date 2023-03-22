d9af8feaafa85a5bae0bc3ed9663d684985a8bc1
Zkarape <zkarapet00@gmail.com>
2023-03-22 12:15:04 +0400

    circle updated

--- circle_class.cpp
+++ circle_class.cpp
@@ -9,10 +9,12 @@ class Circle
 		double	_r;
 		double	_x0;
 		double	_y0;
+	//	double	area;
 	public:
 		void	setRadius(double);
-		void	setCenterx(double);
-		void	setCentery(double);
+		int	setCenterx(double);
+		int	setCentery(double);
+	//	void	area_check(double);
 		double	getRadius() const;
 		double	getCenterx() const;
 		double	getCentery() const;
@@ -34,16 +36,18 @@ void	Circle::setRadius(double radius)
 		_r = radius;
 }
 
-void	Circle::setCenterx(double x0)
+int	Circle::setCenterx(double x0)
 {
 	if (_x0 + _r > 0)
-		_x0 = x0;
+		return (_x0 = x0, 1);
+	return (0);
 }
 
-void	Circle::setCentery(double y0)
+int	Circle::setCentery(double y0)
 {
 	if (_y0 + _r > 0)
-		_y0 = y0;
+		return (_y0 = y0);
+	return (0);
 }
 
 double	Circle::getRadius() const
@@ -61,65 +65,92 @@ double	Circle::getCentery() const
 	return (_y0);
 }
 
-int	belongs_circle(const double x, const double y, const Circle &C) const
+double	area(const Circle &C)
+{
+	double r = C.getRadius();
+	double area = PHI * r * r;
+
+	std::cout << "Are of circle is: " << area << std::endl;
+	return (area);
+}
+
+void	area_check(Circle &C, double k)
 {
 	double x0 = C.getCenterx();
 	double y0 = C.getCentery();
 	double r = C.getRadius();
-	if (pow(x - x0) + pow(y - y0) <= r * r)
+
+	if (x0 + sqrt(k) * r > 0 && y0 + sqrt(k) * r > 0)
+	{
+		C.setRadius(sqrt(k) * r);
+		std::cout << "The new area is: " << area(C) << std::endl;
+	}
+	std::cout << "The area is: " << area(C) << std::endl;
+}
+
+int	belongs_circle(double x, double y, Circle &C) const
+{
+	double x0 = C.getCenterx();
+	double y0 = C.getCentery();
+	double r = C.getRadius();
+
+	if (pow(x - x0, 2) + pow(y - y0, 2) <= r * r)
 		return (1);
 	return (0);
 }
 
-int	num_of_intersections(const Circle &C1, const Circle &C2) const
+double	distance_of_centers(Circle &C1, Circle &C2) const
 {
 	double x01 = C1.getCenterx();
 	double y01 = C1.getCentery();
-	double r1 = C1.getRadius();
 	double x02 = C2.getCenterx();
 	double y02 = C2.getCentery();
+	double	D = 0;
+
+	D = sqrt(pow((x01 - x02), 2) + pow((y01 - y02), 2));
+	std::cout << "Distance between centers is: " << D << std::endl;
+	return (D);
+}
+
+int	num_of_intersections(Circle &C1, Circle &C2) const
+{
+	double x1 = C1.getCenterx();
+	double y1 = C1.getCentery();
+	double r1 = C1.getRadius();
+	double x2 = C2.getCenterx();
+	double y2 = C2.getCentery();
 	double r2 = C2.getRadius();
 	double	D = 0;
 
-	D = (2 * pow((x2 - x1), 2) + (2 * pow((y2 - y1), 2) - 4 * (r1 * r1 - r2 * r2 + x1 * x1 - x2 * x2 + y1 * y1 - y2 * y2);
-	if (r1 == r2 && x01 == x02 && y01 == y02)
+	D = distance_of_centers(C1, C2);
+	if (r1 == r2 && x1 == x2 && y1 == y2)
 		return (-1);
-	if (D == 0)
-		return (1);
-	if (D > 0)
-		return (2);
-	if (D < 0)
+	if (D > r1 + r2)
 		return (0);
+	if (D < r1 + r2)
+		return (2);
+	if (D == abs(r1 - r2))
+		return (1);
+	return (8);
 }
 
-void	length_calc(const Circle &C) const
+void	length_calc(Circle &C) const
 {
 	double r = C.getRadius();
 	
 	std::cout << "The length of circle is: " << 2 * PHI * r << std::endl;
 }
 
-void	area(const Circle &C) const
+void	move_horizontally(Circle &C, double move_step)
 {
-	double r = C.getRadius();
-
-	std::cout << "Are of circle is: " << PHI * r * r << std::endl;
+	C.setCenterx(C.getCenterx() + move_step); 
+	std::cout << "The new x_0 is: " << C.getCenterx() << std::endl;
 }
 
-void	distance_of_centers(const Circle &C1, const Circle &C2) const
+void	move_vertically(Circle &C, double move_step)
 {
-	double x01 = C1.getCenterx();
-	double y01 = C1.getCentery();
-	double x02 = C2.getCenterx();
-	double y02 = C2.getCentery();
-	double	D = 0;
-
-	D = pow((x01 - x02), 2) + pow((y01 - y02), 2);
-	std::cout << "Distance between centers is: " << D << std::endl;
+	C.setCentery(C.getCentery() + move_step);
+	std::cout << "The new y_0 is: " << C.getCentery() << std::endl;
 }
 
-void	move_horizontally(const Circle &C, double move_length) const
-{
-	std::cout << "The new x_0 is: " << C.getCenterx() + move_lenght << std::endl;
-	std::cout << "The new y_0 is: " << C.getCentery() + move_lenght << std::endl;
-}
+
